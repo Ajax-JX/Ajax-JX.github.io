0f0604807d0667bc2403818d4d937faed27d03bc
Ajax-JX <2950788470@qq.com>
2025-01-27 20:50:40 +0800

    Create github上传文件.py

---
+++ other/github上传文件.py
@@ -0,0 +1,88 @@
+import requests
+import json
+import re
+
+headers = {
+    'accept': 'application/json',
+    'accept-encoding': 'gzip, deflate, br',
+    'accept-language': 'zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6',
+    'cookie': '_octo=GH1.1.329557152.1722407951; _device_id=f38f305520b30d1e6de4ab292e00b7fc; saved_user_sessions=171770425%3AF4Prc1U4__ivY74PHKJYusHcy96E7Gjzy45-cgIwwlZeOGiS; user_session=F4Prc1U4__ivY74PHKJYusHcy96E7Gjzy45-cgIwwlZeOGiS; __Host-user_session_same_site=F4Prc1U4__ivY74PHKJYusHcy96E7Gjzy45-cgIwwlZeOGiS; logged_in=yes; dotcom_user=Ajax-JX; color_mode=%7B%22color_mode%22%3A%22auto%22%2C%22light_theme%22%3A%22light%22%2C%22dark_theme%22%3A%22dark%22%7D; cpu_bucket=xlg; preferred_color_mode=dark; tz=Asia%2FShanghai; _gh_sess=GjlJZ2VNOV0z1Bj6A1wVluI%2FJloZDQs7xHruK95xE2WNwn9TwHJCPMip%2BQHgOuoRm3mG7vqM3zO9ef706%2Fjtb9x4UFf5lw%2BevnAD32Mj1hAx%2BZt8CqGDWtpxaQLsS3wwLG44UmcGkMR5EUNDE4t0sVKCStlo07jjCFRz2S7X6Il9Faae0%2BJfySHBI9DRcJZI2zvPf%2FkHAlf8vTJg3M5%2Fbjn1HUSjJOuLllh3NgcexV3xxRR93q6gx0PcKfok0ZkFl0bMZ7vaT3pK8P7H08Rozv9BqZn5InBbfUxP5%2FsbKdEiFAksgjcWuznt%2FpZyzsmapuK6yERGw2yQRSb84j4%2FRtUKvzYdF4kav8b2dYTQelw3Mjhs%2F%2FsYEpa1h5yXX7TrNTl5tLI%2FytWT98BuAyDhGRBscW8%3D--bo8Xrm6WIw1DEPHn--jEHsWSdD8lBiz6ecoJ6xuQ%3D%3D',
+    'if-none-match': 'W/"1358878c4acc25a4bca6e1c2f64b1853"',
+    'referer': 'https://github.com/Ajax-JX/Ajax-JX.github.io/new/main/other',
+    'sec-ch-ua': '"Chromium";v="112", "Microsoft Edge";v="112", "Not:A-Brand";v="99"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': '"Windows"',
+    'sec-fetch-dest': 'empty',
+    'sec-fetch-mode': 'cors',
+    'sec-fetch-site': 'same-origin',
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36 Edg/112.0.1722.58',
+    'x-github-target': 'dotcom',
+    'x-react-router': 'json',
+    'x-requested-with': 'XMLHttpRequest'
+   }
+
+#鍒涘缓鏂囦欢鐨勪俊鎭痷rl=https://github.com/Ajax-JX/Ajax-JX.github.io/new/main/other
+#鍒犻櫎鏂囦欢鐨勪俊鎭痷rl=https://github.com/Ajax-JX/Ajax-JX.github.io/delete/main/other/title
+
+def get_info(url):
+   response = requests.get(url, headers=headers, verify=False)
+   return json.loads(response.text)
+
+
+def post_file(filename,filecontent):
+   info_url="https://github.com/Ajax-JX/Ajax-JX.github.io/new/main/other"
+   a=get_info(info_url)
+   csrf_tokens=a["payload"]["csrf_tokens"]["/Ajax-JX/Ajax-JX.github.io/create/main/other"]["post"]
+   commit_oid=a["payload"]["refInfo"]["currentOid"]
+   url="https://github.com/Ajax-JX/Ajax-JX.github.io/create/main/other"
+   data = {
+    'message': 'Create '+filename,
+    'placeholder_message': 'Create '+filename,
+    'description': '',
+    'commit-choice': 'direct',
+    'target_branch': 'main',
+    'quick_pull': '',
+    'guidance_task': '',
+    'commit': commit_oid,
+    'same_repo': '1',
+    'pr': '',
+    'content_changed': 'true',
+    'authenticity_token': csrf_tokens
+   }
+   files = {
+    'filename': (None, 'other/'+filename),
+    'new_filename': (None, 'other/'+filename),
+    'value': (None, filecontent)
+   }
+   response = requests.post(url, data=data, files=files,verify=False,headers=headers).text
+   return response
+
+
+def delete_file(filename):
+   info_url="https://github.com/Ajax-JX/Ajax-JX.github.io/delete/main/other/title"
+   a=get_info(info_url)
+   csrf_tokens=a["payload"]["csrf_tokens"]["/Ajax-JX/Ajax-JX.github.io/blob/main/other/"+filename]["delete"]
+   commit_oid=a["payload"]["refInfo"]["currentOid"]
+   url="https://github.com/Ajax-JX/Ajax-JX.github.io/blob/main/other/"+filename
+   data = {
+           'message': 'Delete other/'+filename, 
+           'placeholder_message': 'Delete other/'+filename, 
+           'description': '', 
+           'commit-choice': 'direct', 
+           'target_branch': 'main', 
+           'quick_pull': '', 
+           'guidance_task': '', 
+           'commit': commit_oid, 
+           'same_repo': '1', 
+           'pr': '', 
+           '_method': 'delete', 
+           'authenticity_token': csrf_tokens
+           }
+   response = requests.post(url, data=data, verify=False,headers=headers).text
+   return response
+
+def get_file():
+    list=requests.get("https://github.com/Ajax-JX/Ajax-JX.github.io/tree-commit-info/main/other",verify=False,headers=headers).text
+    pattern = r'"([^"]*)":{"oid"'
+    matches = re.findall(pattern, list)
+    return matches
